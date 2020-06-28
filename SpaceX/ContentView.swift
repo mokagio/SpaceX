9d6eb36d433001be75a27e381b12f585ab63ddfb
Gio Lodi <giovanni.lodi42@gmail.com>
2020-06-28 22:16:41 +1000

    New project SwiftUI project with Xcode 11.5

---
+++ SpaceX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceX
+//
+//  Created by Gio on 28/6/20.
+//  Copyright © 2020 mokagio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
