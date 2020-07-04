575e8c4884c7526ea22d2f71c646fe20d840379c
Gio Lodi <giovanni.lodi42@gmail.com>
2020-07-04 21:31:20 +1000

    Add extension on Collection for safe subscript access

---
+++ SpaceX/Collection+SafeAccess.swift
@@ -0,0 +1,7 @@
+extension Collection {
+
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
