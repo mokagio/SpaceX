c17e04b5515056bc4c20773fafd782386b1a7164
Gio Lodi <giovanni.lodi42@gmail.com>
2020-07-05 21:38:06 +1000

    Add fake sections to SwiftUI implementation

--- SpaceX/ContentView.swift
+++ SpaceX/ContentView.swift
@@ -2,11 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
 
+    // Got the code to do the sections from here https://softauthor.com/swiftui-list-section-row/
+    struct SectionSource<T>: Identifiable {
+        let id = UUID()
+        let title: String
+        let items: [T]
+    }
+
     @ObservedObject var fetcher = LaunchFetcher()
 
     var body: some View {
-        List(fetcher.launches) { launch in
-            Text(launch.name)
+        List {
+            ForEach(
+                [
+                    SectionSource(title: "a", items: fetcher.launches),
+                    SectionSource(title: "b", items: fetcher.launches)
+                ]
+            ) { section in
+                Section(header: Text(section.title)) {
+                    ForEach(section.items) { item in
+                        Text(item.name)
+                    }
+                }
+            }
         }
         .onAppear {
             guard let url = Bundle.main.url(forResource: "past_launches", withExtension: "json") else { return }
