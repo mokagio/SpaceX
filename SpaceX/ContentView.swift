3043f6df19fac5a643e646d53c52c14c15ab89ce
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-10 20:40:31 +1000

    Extract view model into dedicated file

--- SpaceX/ContentView.swift
+++ SpaceX/ContentView.swift
@@ -1,41 +1,9 @@
 import Combine
 import SwiftUI
 
-class LaunchViewModel: ObservableObject {
-
-    private let launchFetcher: LaunchFetcher
-    private var bag = Set<AnyCancellable>()
-
-    // TODO: Make a `RemoteData`
-    @Published var launches: Result<[Launch], Error>?
-
-    init(fetcher: LaunchFetcher = LaunchFetcher()) {
-        launchFetcher = fetcher
-    }
-
-    // I'm not sure I like how this is bind to the API of SwiftUI.
-    func onAppear() {
-        launchFetcher.loadFromTheNet()
-            // This delay is so that I can notice the loading when there's URLSession caching in place
-            .delay(for: .seconds(2), scheduler: DispatchQueue.global(qos: .background))
-            .receive(on: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { [weak self] completion in
-                    guard case .failure(let error) = completion else { return print("Completed") }
-                    self?.launches = .failure(error)
-                },
-                receiveValue: { [weak self] launches in
-                    print("Got \(launches.count) results")
-                    self?.launches = .success(launches)
-                }
-            )
-            .store(in: &bag)
-    }
-}
-
 struct ContentView: View {
 
-    @ObservedObject var viewModel: LaunchViewModel
+    @ObservedObject var viewModel: LaunchesListViewModel
 
     var body: some View {
         switch viewModel.launches {
@@ -62,7 +30,7 @@ struct ContentView: View {
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: LaunchViewModel())
+        ContentView(viewModel: LaunchesListViewModel())
     }
 }
 #endif
