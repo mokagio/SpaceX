6659ab7f70e78b51bb0d0fb2707e5a6fb08cef29
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-10 21:10:33 +1000

    Track LoadableViewModel protocol, although seems a dead end at the moment

---
+++ SpaceX/LoadableViewModel.swift
@@ -0,0 +1,21 @@
+import Combine
+
+// I thought this was a good idea, but then I couldn't use it as an `@ObservedObeject` in a `View`
+// because it depends on `Self`.
+//
+// I might have more luck with a concrete class, along the lines of `RemoteDataViewModel<Resource>`,
+// but this is not the time to work on abstractions that I don't know if I'll need. YAGNI!
+
+protocol LoadableViewModel {
+
+    associatedtype Resource
+
+    var publishedResource: Published<RemoteData<Resource, Error>>.Publisher { get }
+}
+
+extension LaunchesListViewModel: LoadableViewModel {
+
+    typealias Resource = [Launch]
+
+    var publishedResource: Published<RemoteData<Resource, Error>>.Publisher { $launches }
+}
