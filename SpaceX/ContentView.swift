91ed0e55ab2419ea5ddebf5d53997155e2e9e22a
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-10 20:58:36 +1000

    Decompose ContentView in smaller views

--- SpaceX/ContentView.swift
+++ SpaceX/ContentView.swift
@@ -1,6 +1,9 @@
 import Combine
 import SwiftUI
 
+// Since my views are still little, I'm trying this unusual approach (for me at least) of having
+// all the views in the same file.
+
 struct ContentView: View {
 
     @ObservedObject var viewModel: LaunchesListViewModel
@@ -8,20 +11,54 @@ struct ContentView: View {
     var body: some View {
         switch viewModel.launches {
         case .notAsked, .loading:
-            Text("Loading...")
+            LoadingView()
         case .failure(let error):
-            Text(error.localizedDescription)
+            ErrorView(error: error)
         case .success(let launches):
-            List {
-                ForEach(groupLaunchesIntoSectionsByName(launches)) { section in
-                    Section(header: Text(section.title)) {
-                        ForEach(section.items) { item in
-                            Text(item.name)
-                        }
+            LaunchesListView(launches: launches)
+        }
+    }
+}
+
+struct LoadingView: View {
+
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct ErrorView: View {
+
+    let error: Error
+
+    var body: some View {
+        Text(error.localizedDescription)
+    }
+}
+
+struct LaunchesListView: View {
+
+    let launches: [Launch]
+
+    var body: some View {
+        List {
+            ForEach(groupLaunchesIntoSectionsByName(launches)) { section in
+                Section(header: Text(section.title)) {
+                    ForEach(section.items) { item in
+                        LaunchCellView(launch: item)
                     }
                 }
             }
         }
+    }
+}
+
+struct LaunchCellView: View {
+
+    let launch: Launch
+
+    var body: some View {
+        Text(launch.name)
     }
 }
 
