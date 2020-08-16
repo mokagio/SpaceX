ebf7a480436311501cbc4f885c73729cc761aa8a
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-16 22:27:20 +1000

    Only load the SwiftUI interface
    Originally, I wanted to use this as an experiment to compare UIKit and
    SwiftUI stuff and how to use business logic across both, but right now
    I'm more interested in learning more about how to test SwiftUI and
    Combine.

--- SpaceX/SceneDelegate.swift
+++ SpaceX/SceneDelegate.swift
@@ -26,6 +26,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let launchesContainerView = LaunchesListContainerView(viewModel: viewModel)
             .onAppear { [unowned self] in self.viewModel.onAppear() }
 
+        /*
+
+         Originally, I wanted to use this as an experiment to compare UIKit and SwiftUI stuff and
+         how to use business logic across both, but right now I'm more interested in learning more
+         about how to test SwiftUI and Combine.
+
         let tab = UITabBarController()
         tab.viewControllers = [
             UIHostingController(rootView: launchesContainerView)
@@ -37,6 +43,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         _ = tab.view
 
         window?.rootViewController = tab
+         */
+
+        let rootViewController = UIHostingController(rootView: launchesContainerView)
+            .with(title: "Swift UI")
+
+        window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
 
