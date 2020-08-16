c9846f386a32e0b76f5ae48dd23d4e409dadb353
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-16 20:51:04 +1000

    Add test for Collection+SafeAccessTests

---
+++ SpaceXTests/Collection+SafeAccessTests.swift
@@ -0,0 +1,15 @@
+@testable import SpaceX
+import XCTest
+
+class CollectionSafeAccessTests: XCTestCase {
+
+    func testAccessInBoundsReturnsValue() {
+        XCTAssertEqual([1, 2, 3][safe: 0], 1)
+        XCTAssertEqual([1, 2, 3][safe: 2], 3)
+    }
+
+    func testOutOfBoundAccessReturnsNil() {
+        XCTAssertNil([1, 2, 3][safe: 3])
+        XCTAssertNil([1, 2, 3][safe: -1])
+    }
+}
