fd00dbcb87275d8e6be21d3dc2c7d6f93c45f30d
Gio Lodi <giovanni.lodi42@gmail.com>
2020-08-17 21:35:11 +1000

    Clean up XCTestCase+Combine

--- SpaceXTests/XCTestCase+Combine.swift
+++ SpaceXTests/XCTestCase+Combine.swift
@@ -2,22 +2,6 @@ import Combine
 import XCTest
 
 extension XCTestCase {
-    func assert<T, E>(_ future: Future<T, E>, receivesValue onValue: @escaping (T) -> Void) {
-        let expectation = XCTestExpectation()
-
-        _ = future
-            .sink(
-                receiveCompletion: { completion in
-                    switch completion {
-                    case .finished: expectation.fulfill()
-                    case .failure(let error): XCTFail(error.localizedDescription)
-                    }
-                },
-                receiveValue: { onValue($0) }
-            )
-
-        wait(for: [expectation], timeout: 0.1)
-    }
 
     func assertReceivesValue<T, E>(_ future: AnyPublisher<T, E>, onValue: @escaping (T) -> Void) {
         let expectation = XCTestExpectation()
@@ -28,8 +12,6 @@ extension XCTestCase {
                     switch completion {
                     case .finished: expectation.fulfill()
                     case .failure(let error):
-                        // TODO: this failure is not reported inline, nor by the new Xcode 12 drill
-                        // into error grey reporter
                         XCTFail("Expected to receive value, failed with: \(error.localizedDescription)")
                     }
                 },
@@ -54,8 +36,6 @@ extension XCTestCase {
                     }
                 },
                 receiveValue: { value in
-                    // TODO: this failure is not reported inline, nor by the new Xcode 12 drill
-                    // into error grey reporter
                     XCTFail("Expected to fail, received value: \(value)")
                 }
             )
