4ea5c19330fffd10f43614c360ea6b9e50d95db1
Gio Lodi <giovanni.lodi42@gmail.com>
2020-10-15 16:18:03 +1100

    Add note regarding why FavoritesController wastefully wraps FavoritesStoring

--- SpaceX/FavoritesListViewModel.swift
+++ SpaceX/FavoritesListViewModel.swift
@@ -6,7 +6,15 @@ extension FavoritesList {
 
         @Published var launches: [Launch]
 
+        // FavoritesController @Publishes favorites, which it reads from FavoritesStoring.
+        // I need the extra layer because I don't know how to define a protocol that @Publishes
+        // stuff, so either the storage layer uses a publisher, or we need to "wrap" it into another
+        // type that @Publishes for it.
+        //
+        // The need for a protocol is for testability. In the tests, I want to be able to simulate
+        // the different storage behaviors.
         let favoritesController: FavoritesController
+
         private var cancellables = Set<AnyCancellable>()
 
         init(favoritesController: FavoritesController) {
